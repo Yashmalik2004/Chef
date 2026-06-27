632aa998d278428969a5eed951e6211eebf78e02
Yashmalik2004 <yashmalik4832004@gmail.com>
2026-06-27 23:17:14 +0530

    Initialised project - Called GROQ API

--- agent/graph.py
+++ agent/graph.py
@@ -5,6 +5,19 @@ from langchain_groq import ChatGroq
 
 llm = ChatGroq(model="openai/gpt-oss-120b")
 
-resp = llm.invoke("Who invented Kriya yoga. answer in 1 sentence")
+from pydantic import BaseModel, Field
 
-print(resp.content)
+user_prompt = "create a simple calculator web application"
+
+prompt = f"""
+Your are the Planner agent. Convert the user prompt into a COMPLETE engineering project plan
+
+User reuest: {user_prompt}
+"""
+
+class Plan(BaseModel):
+    pass
+
+resp = llm.with_structured_output(Schema).invoke(prompt)
+
+print(resp)
